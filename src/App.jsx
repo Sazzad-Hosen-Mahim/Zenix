192bd1b78143359e7536dab146026c099e928b2c
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2025-01-11 00:22:13 +0600

    hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Hero from "./components/Hero";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 };
