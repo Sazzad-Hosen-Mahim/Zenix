c274010c13de3c88edddc81f354201af6b1cb0d5
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2025-01-14 23:59:01 +0600

    navbar completed with animation and scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Hero from "./components/Hero";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>
