16034bf7695248371528d0997856e6994f87428f
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2025-01-15 15:42:06 +0600

    features section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Features />
     </main>
   );
 };
